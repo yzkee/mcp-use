74c1bcfdd3b7c8ab755179aef632cf45d5c50df7
pietrozullo <pietro.zullo@gmail.com>
2025-04-14 11:43:49 +0200

    Fix import

--- mcp_use/agents/__init__.py
+++ mcp_use/agents/__init__.py
@@ -8,11 +8,7 @@ that are pre-configured for using MCP tools.
 from .base import BaseAgent
 from .mcpagent import MCPAgent
 
-# Import components
-from .server_manager import ServerManager
-
 __all__ = [
     "BaseAgent",
     "MCPAgent",
-    "ServerManager",
 ]
