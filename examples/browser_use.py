832bcd033bbc7007a0a3a0840434fbce97aac19f
pietrozullo <pietro.zullo@gmail.com>
2025-07-11 10:31:07 -0700

    simplified example

--- examples/browser_use.py
+++ examples/browser_use.py
@@ -8,7 +8,6 @@ Special thanks to https://github.com/microsoft/playwright-mcp for the server.
 """
 
 import asyncio
-import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
@@ -21,14 +20,14 @@ async def main():
     # Load environment variables
     load_dotenv()
 
+    config = {
+        "mcpServers": {"playwright": {"command": "npx", "args": ["@playwright/mcp@latest"], "env": {"DISPLAY": ":1"}}}
+    }
     # Create MCPClient from config file
-    client = MCPClient.from_config_file(os.path.join(os.path.dirname(__file__), "browser_mcp.json"))
+    client = MCPClient(config=config)
 
     # Create LLM
     llm = ChatOpenAI(model="gpt-4o")
-    # llm = init_chat_model(model="llama-3.1-8b-instant", model_provider="groq")
-    # llm = ChatAnthropic(model="claude-3-")
-    # llm = ChatGroq(model="llama3-8b-8192")
 
     # Create agent with the client
     agent = MCPAgent(llm=llm, client=client, max_steps=30)
